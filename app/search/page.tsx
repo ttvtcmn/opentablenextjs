9641ff21cafc5b0fb4fc5f9511edda54345b972c
ttvtcmn <ttvtcmn@gmail.com>
2023-07-06 15:43:27 +0700

    Day 07: Add Query Params

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,18 +2,71 @@ import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
 import { Metadata } from "next";
+import { PRICE, PrismaClient } from "@prisma/client";
 
 export const metadata: Metadata = {
   title: "Search | Opentable",
 };
-export default function Search() {
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantByCity = (city: string | undefined) => {
+  if (!city) return prisma.restaurant.findMany();
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      price: true,
+      cuisine: true,
+      location: true,
+      slug: true,
+    },
+  });
+};
+
+const fetchLocation = async () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisines = async () => {
+  return prisma.cuisine.findMany();
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
+}) {
+  const restaurants = await fetchRestaurantByCity(searchParams.city);
+  const locations = await fetchLocation();
+  const cuisines = await fetchCuisines();
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar
+          locations={locations}
+          cuisines={cuisines}
+          searchParams={searchParams}
+        />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            <>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+              ))}
+            </>
+          ) : (
+            <p>Sorry, we found no restaurant in this area</p>
+          )}
         </div>
       </div>
     </>
